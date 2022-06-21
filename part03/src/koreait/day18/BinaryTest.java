6211add076af941a5cce30f5da30529adaeac1bb
sysoHoon <shp000915@gmail.com>
2022-06-21 16:29:17 +0900

    Git연습소스

---
+++ part03/src/koreait/day18/BinaryTest.java
@@ -0,0 +1,12 @@
+package koreait.day18;
+
+public class BinaryTest {
+	public static void main(String[] args) {
+		
+		System.out.println("10진수\t2진수\t 16진");
+		for(int i = 0; i < 100; i++) {
+			System.out.println(String.format("%d\t%s\t %s", i, Integer.toBinaryString(i),
+									Integer.toHexString(i)));	
+		}
+	}
+}
